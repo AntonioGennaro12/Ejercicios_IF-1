adaa88582f70dd961415058dd3f703bd41010e2e
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-14 23:42:09 -0300

    ejercicio 1 Ver 2

--- ejercicio1.js
+++ ejercicio1.js
@@ -2,16 +2,23 @@
 Verificar nombre de usurio y contraseña
 contra usuario registrado
 informar el resultado del intento de login 
+Se agrega otra cuenta registrada y se verifica
+contra las 2, solo un usuario por vez.
 */
 
-const USUARIO_NOMBRE_REGISTRADO = "pepe";
-const USUARIO_CONTRASENIA_REGISTRADO = "asd123"
+const USUARIO1_NOMBRE_REGISTRADO = "pepe";
+const USUARIO2_NOMBRE_REGISTRADO = "jose";
+const USUARIO1_CONTRASENIA_REGISTRADO = "asd123"
+const USUARIO2_CONTRASENIA_REGISTRADO = "qwe456"
 
 let usuarioNombreIngresado = prompt("Ingrese Nombre de Ususario");
 let usuarioCOntraseniaIngresado = prompt ("Ingrese su contraseña");
 
-if ((usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO) && (usuarioCOntraseniaIngresado == USUARIO_CONTRASENIA_REGISTRADO)) {
+if ((usuarioNombreIngresado == USUARIO1_NOMBRE_REGISTRADO) && (usuarioCOntraseniaIngresado == USUARIO1_CONTRASENIA_REGISTRADO)) {
     document.write ('Bienvenido a su Cuenta Sr. "' + usuarioNombreIngresado +'"');
+}
+else if ((usuarioNombreIngresado == USUARIO2_NOMBRE_REGISTRADO) && (usuarioCOntraseniaIngresado == USUARIO2_CONTRASENIA_REGISTRADO)) {
+    document.write ('Bienvenido a su Cuenta Sr. "' + usuarioNombreIngresado +'"'); 
 }
 else {
     document.write ("Nombre de Usuario y/o Contrseña incorrectos!!" ); 
