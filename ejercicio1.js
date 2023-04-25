330c434aa81b14a15f80d8e7684125de0ade655f
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-25 09:45:48 -0300

    Ejercicio 1 IF v3 c/inp y button

--- ejercicio1.js
+++ ejercicio1.js
@@ -4,22 +4,34 @@ contra usuario registrado
 informar el resultado del intento de login 
 Se agrega otra cuenta registrada y se verifica
 contra las 2, solo un usuario por vez.
+Se agrega uso de elementos "Label" , "Input" y "button"
 */
+const inpUsuario        = document.querySelector("#nombre-usuario")
+const impContrasenia    = document.querySelector("#nombre-contrasenia")
+const miParrafo         = document.querySelector("#mi-parrafo")
 
-const USUARIO1_NOMBRE_REGISTRADO = "pepe";
-const USUARIO2_NOMBRE_REGISTRADO = "jose";
-const USUARIO1_CONTRASENIA_REGISTRADO = "asd123"
-const USUARIO2_CONTRASENIA_REGISTRADO = "qwe456"
+const USUARIO1_NOMBRE_REGISTRADO        = "Antonio12";
+const USUARIO2_NOMBRE_REGISTRADO        = "JuanPedro";
+const USUARIO1_CONTRASENIA_REGISTRADO   = "clave055"
+const USUARIO2_CONTRASENIA_REGISTRADO   = "gatopardo"
 
-let usuarioNombreIngresado = prompt("Ingrese Nombre de Ususario");
-let usuarioCOntraseniaIngresado = prompt ("Ingrese su contraseña");
-
-if ((usuarioNombreIngresado == USUARIO1_NOMBRE_REGISTRADO) && (usuarioCOntraseniaIngresado == USUARIO1_CONTRASENIA_REGISTRADO)) {
-    document.write ('Bienvenido a su Cuenta Sr. "' + usuarioNombreIngresado +'"');
+function impUsuario (nombre){
+    miParrafo.textContent = "Bienvenido a su Cuenta Usuario: " + nombre +"";
 }
-else if ((usuarioNombreIngresado == USUARIO2_NOMBRE_REGISTRADO) && (usuarioCOntraseniaIngresado == USUARIO2_CONTRASENIA_REGISTRADO)) {
-    document.write ('Bienvenido a su Cuenta Sr. "' + usuarioNombreIngresado +'"'); 
-}
-else {
-    document.write ("Nombre de Usuario y/o Contrseña incorrectos!!" ); 
+
+function tomaInfo ()  {
+    let usuarioNombreIngresado      = inpUsuario.value ;
+    let usuarioCOntraseniaIngresado = impContrasenia.value ;
+    
+    if ((usuarioNombreIngresado == USUARIO1_NOMBRE_REGISTRADO) && 
+                (usuarioCOntraseniaIngresado == USUARIO1_CONTRASENIA_REGISTRADO)) {
+        impUsuario (usuarioNombreIngresado);
+    }
+    else if ((usuarioNombreIngresado == USUARIO2_NOMBRE_REGISTRADO) && 
+                (usuarioCOntraseniaIngresado == USUARIO2_CONTRASENIA_REGISTRADO)) {
+        impUsuario (usuarioNombreIngresado); 
+    }
+    else {
+        miParrafo.textContent =  "Nombre de Usuario y/o Contrsenia incorrectos!!"; 
+    }
 }
